3cd395753d43e529542daa71ed705ad354d12389
Tortuvshin <toroo.byamba@gmail.com>
2016-01-02 15:44:11 +0000

    new template blog.html blogs.html remove temlates hello hello_class

--- blog/views.py
+++ blog/views.py
@@ -14,27 +14,6 @@ def blog(request, blog_id=1):
     return render_to_response('blog.html',
     {'blog':Blog.objects.get(id=blog_id)})
 
-def hello(request):
-    name = "Toroo"
-    html = u"<html><body>Сайн уу %s. ажиллаж байна</body></html> "%name
-    return HttpResponse(html)
-    
-def hello_template(request):
-    name = "Toroo"
-    mytemplate = get_template('hello.html')
-    html = mytemplate.render(Context({'name' : name}))
-    return HttpResponse(html)
-    
-def hello_template_simple(request):
-    name = "Toroo"
-    return render_to_response('hello.html', {'name' : name})
-    
-class HelloTemplate(TemplateView):
-    template_name = 'hello_class.html'
-    def get_context_data(self, **kwargs):
-        context = super(HelloTemplate, self).get_context_data(**kwargs)
-        context['name'] = 'Toroo'
-        return context
     
     
     
