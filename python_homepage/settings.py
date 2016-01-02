77af255600028757a34b089cb1f0654133b9742b
Tortuvshin <toroo.byamba@gmail.com>
2016-01-02 16:26:34 +0000

    new template base.html update blogs

--- python_homepage/settings.py
+++ python_homepage/settings.py
@@ -59,7 +59,8 @@ ROOT_URLCONF = 'python_homepage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, '/blog/templates'),
+                os.path.join(BASE_DIR, 'templates',)],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
